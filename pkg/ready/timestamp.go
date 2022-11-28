3f9eb924661a26e12056f83a1f24f23d6524fcd4
Raul Sevilla <rsevilla@redhat.com>
2022-11-28 16:04:07 +0100

    Connect to DB in background and avoid exitting on query error
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- pkg/ready/timestamp.go
+++ pkg/ready/timestamp.go
@@ -1,4 +1,4 @@
-package timestamp
+package ready
 
 import (
 	"fmt"
@@ -6,7 +6,6 @@ import (
 	"time"
 
 	"github.com/rsevilla87/perfapp/internal/perf"
-	"github.com/rsevilla87/perfapp/pkg/utils"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -17,13 +16,13 @@ var Tables = map[string]string{"ts": "CREATE TABLE IF NOT EXISTS ts (date TIMEST
 func Handler(w http.ResponseWriter, r *http.Request) {
 	log.Info("Inserting timestamp record in table")
 	now := time.Now()
-	elapsed := time.Now().Sub(now)
 	insert := fmt.Sprintf("INSERT INTO ts VALUES ('%s')", now.Format(time.RFC3339))
 	if err := perf.QueryDB(insert); err != nil {
-		utils.ErrorHandler(err)
+		w.WriteHeader(http.StatusServiceUnavailable)
+		fmt.Fprintln(w, err.Error())
 	} else {
 		fmt.Fprintln(w, "Ok")
-		log.Printf("Timestamp inserted in %v ns", elapsed.Nanoseconds())
-		perf.HTTPRequestDuration.Observe(elapsed.Seconds())
+		log.Printf("Timestamp inserted in %v ns", time.Since(now).Nanoseconds())
+		perf.HTTPRequestDuration.Observe(time.Since(now).Seconds())
 	}
 }
